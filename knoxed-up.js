016828c2d13e41e96e5cdadb632bc4b6c75ab20d
Mark Armendariz <src@enobrev.com>
2013-10-23 15:41:49 -0700

    retry putstream on 400+

--- knoxed-up.js
+++ knoxed-up.js
@@ -555,7 +555,7 @@
                                 syslog.warn(oLog);
                                 this.putStream(sFrom, sTo, oHeaders, fCallback, iRetries + 1);
                             }
-                        } else if(oResponse.statusCode == 500) {
+                        } else if(oResponse.statusCode >= 400) {
                             oLog.error   = new Error('S3 Error Code ' + oResponse.statusCode);
                             oLog.action += '.request.500.retry';
                             if (iRetries > 3) {
@@ -565,9 +565,6 @@
                                 syslog.warn(oLog);
                                 this.putStream(sFrom, sTo, oHeaders, fCallback, iRetries + 1);
                             }
-                        } else if(oResponse.statusCode >= 400) {
-                            oLog.error = new Error('S3 Error Code ' + oResponse.statusCode);
-                            fDone(fCallback, oLog.error);
                         } else {
                             oLog.action += '.done';
                             fDone(fCallback, null, sTo);
